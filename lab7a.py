90a7cc55f6eca09a8d7789aeb8f8c2fda65b092c
Sunny <mbasion@myseneca.ca>
2024-11-24 15:51:21 -0500

    Added lab7a.py script

--- lab7a.py
+++ lab7a.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+
 class Time:
-    """Simple object type for time of the day.
-    data attributes: hour, minute, second
-    """
-    def __init__(self,hour=12,minute=0,second=0):
-        """constructor for time object""" 
-        self.hour = hour
-        self.minute = minute
-        self.second = second
+   """Simple object type for time of the day.
+      data attributes: hour, minute, second
+   """
+   def __init__(self,hour=12,minute=0,second=0):
+       """constructor for time object""" 
+       self.hour = hour
+       self.minute = minute
+       self.second = second
 
 def format_time(t):
     """Return time object (t) as a formatted string"""
-    return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}'
+    return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}' 
 
 def sum_times(t1, t2):
     """Add two time objests and return the sum."""
@@ -20,13 +20,28 @@ def sum_times(t1, t2):
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+
+  #Carry over for seconds
+    if sum.second >= 60:
+        sum.minute += sum.second // 60  # Carryover to minutes
+        sum.second = sum.second % 60  # Only keep the remainder
+
+    #Carry over for minutes
+    if sum.minute >= 60:
+        sum.hour += sum.minute // 60  # Carryover to hours
+        sum.minute = sum.minute % 60  # Only keep the remainder 
+
+    #Ensuring the hour is within a valid range
+    if sum.hour >= 24:
+        sum.hour = sum.hour % 24  # what to do if the hour exceeds 24, wrap around. 
+
     return sum
 
 def valid_time(t):
     """check for the validity of the time object attributes:
-        24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
+       24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
     if t.hour < 0 or t.minute < 0 or t.second < 0:
-        return False
+       return False
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
-        return False
+       return False
     return True
